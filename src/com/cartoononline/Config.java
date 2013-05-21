682ced94ddba8a675a0e3949b0789861d21ff1ba
guoqing.sun <guoqing.sun@renren-inc.com>
2013-05-21 16:38:32 +0800

    修改配置

--- src/com/cartoononline/Config.java
+++ src/com/cartoononline/Config.java
@@ -17,8 +17,8 @@ public class Config {
     public static final String INI_FILE = "info.ini";
     
     public static int INDEX = 0;
-    public static final String[] YOUMI_APP_ID = new String[] { "e3ff884a6cc7b41a", "556af9dfcb127141", "556af9dfcb127141" };
-    public static final String[] YOUMI_APP_SECRET_KEY = new String[] { "26ab38202db7d6fc", "a3b8473046176ba2", "a3b8473046176ba2" };
+    public static final String[] YOUMI_APP_ID = new String[] { "e3ff884a6cc7b41a", "556af9dfcb127141", "eb90ce5fc3bca339" };
+    public static final String[] YOUMI_APP_SECRET_KEY = new String[] { "26ab38202db7d6fc", "a3b8473046176ba2", "af8d30ec5dbc40aa" };
     public static final String[] PACKAGE_NAME = new String[] { "com.cartoononline", "com.michael.manhua", "com.michael.rosi" };
     public static final String[] PUSH_TAG = new String[] { "yaojing", "manhua", "rosi" };
     public static final String[] DOMAIN_NAME = new String[] { "psave", "xiee", "rosi" };
