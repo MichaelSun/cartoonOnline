0cd9e9a95ad458fd0fe03079c901fe79f079ec58
guoqing.sun <guoqing.sun@renren-inc.com>
2013-06-18 21:28:29 +0800

    更新编译脚本

--- build.py
+++ build.py
@@ -133,6 +133,9 @@ def __main(args):
 
         print 'backup the build target %s/%s success >>>>>>>>' % (target, target_apk)
 
+    print 'after build for new package : %s, just reset code ' % new_package
+    os.system('git reset --hard HEAD')
+
     return True
 
 if __name__ == '__main__':
