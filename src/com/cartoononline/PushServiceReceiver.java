7407e5dc782c80bbb53b77f85fe875abaa081b1f
guoqing.sun <guoqing.sun@renren-inc.com>
2013-09-24 10:34:46 +0800

    更新了编译错误

--- src/com/cartoononline/PushServiceReceiver.java
+++ src/com/cartoononline/PushServiceReceiver.java
@@ -4,22 +4,20 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
-import com.baidu.android.pushservice.PushConstnts;
-import com.baidu.android.pushservice.PushManager;
 
 public class PushServiceReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        if (intent.getAction().equals(PushConstants.ACTION_RECEIVER_NOTIFICATION_CLICK)) {
-            Intent i = new Intent();
-            i.setClass(context, CartoonSplashActivity.class);
-            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            i.putExtra(CartoonSplashActivity.KEY_FORECE_DOWNLOAD_SHOW, true);
-            context.startActivity(i);
-        } else if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
-            PushManager.startWork(context, PushConstants.LOGIN_TYPE_API_KEY, Utils.getMetaValue(context, "api_key"));
-        }
+//        if (intent.getAction().equals(PushConstants.ACTION_RECEIVER_NOTIFICATION_CLICK)) {
+//            Intent i = new Intent();
+//            i.setClass(context, CartoonSplashActivity.class);
+//            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//            i.putExtra(CartoonSplashActivity.KEY_FORECE_DOWNLOAD_SHOW, true);
+//            context.startActivity(i);
+//        } else if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
+//            PushManager.startWork(context, PushConstants.LOGIN_TYPE_API_KEY, Utils.getMetaValue(context, "api_key"));
+//        }
     }
 
 }
